800531f8e70f53d2ebcbd9ae64fc4a00eb7be211
UniqueDevStorm <storm@uniquecode.team>
2021-02-15 21:39:06 +0900

    refactor: reformat code

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,5 @@
-// import {CTSClient} from "command.ts-rewrite";
-// import {Manager, Payload} from 'erela.js';
-// import {Intents} from "discord.js";
-
 process.on('unhandledRejection', console.error)
 process.on('uncaughtException', console.error)
-
-// declare module 'discord.js' {
-//     interface Client {
-//         manager: Manager
-//     }
-// }
-
-// const client = new CTSClient({prefix: 'S'})
-
-
-
-// client.loadExtension(require.resolve('./extensions/Music'), true)
-
-// client.login('Your Token' /* 여기 토큰 */)
 
 import SystemClient from "./client";
 
